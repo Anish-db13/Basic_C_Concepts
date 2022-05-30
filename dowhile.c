f3a600272e722557122f53ab1444a3476cd26d2d
Anish Dhondi <71207040+Anish-db13@users.noreply.github.com>
2022-05-30 22:16:24 +0530

    Do While Loop

---
+++ dowhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,i=0;
+    printf("Enter a number\n");
+    scanf("%d",&num);
+    do
+    {
+        printf("%d\n",i);
+        i=i+1;
+    }while(i<num);
+    return 0;
+}
